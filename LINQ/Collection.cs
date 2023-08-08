28214899369951a83f3a597ae90a1970d6694bb2
Upendra Sahu <upendrasahu1199@gmail.com>
2023-08-08 08:38:47 +0530

    LINQ for find even number in List

---
+++ LINQ/Collection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LINQ
+{
+    internal class Collection
+    {
+        List<int> numbers = new List<int> { 1, 5, 3, 6 };
+        public void findEven()
+        {
+           
+            // LINQ query for find even number 
+            var evenNumber = from num in numbers
+                             where num % 3 == 0
+                             select num;
+
+            foreach (var num in evenNumber)
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+        public void NumberHavingFactor3()
+        {
+            // LINQ query for find the number having factor 3
+            var Factor3 = from num in numbers where num % 3 == 0 select num;
+            foreach ( var num in Factor3)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Collection collection = new Collection();
+            collection.findEven();
+            collection.NumberHavingFactor3();
+        }
+    }
+}
